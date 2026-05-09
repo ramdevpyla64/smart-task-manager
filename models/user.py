e9c896503a4269c939eb16a17a14e5520ad56e42
ramdevpyla64 <ramdevpyla64@gmail.com>
2026-05-09 21:17:48 +0530

    Add files via upload

---
+++ models/user.py
@@ -0,0 +1,31 @@
+from extensions import db
+from flask_login import UserMixin
+
+class User(UserMixin, db.Model):
+    tasks = db.relationship(
+    'Task',
+    backref='user',
+    lazy=True
+)
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    username = db.Column(
+        db.String(100),
+        nullable=False
+    )
+
+    email = db.Column(
+        db.String(120),
+        unique=True,
+        nullable=False
+    )
+
+    password = db.Column(
+        db.String(200),
+        nullable=False
+    )
+
+    def __repr__(self):
+        return f"<User {self.username}>"
